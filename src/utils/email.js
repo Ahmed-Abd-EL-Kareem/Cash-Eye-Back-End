e9d5fcb8ca5c4c1b57dde427092469927d7eaaba
Ahmed-Abd-EL-Kareem <137029582+Ahmed-Abd-EL-Kareem@users.noreply.github.com>
2026-06-11 20:59:41 +0300

    fix email

--- src/utils/email.js
+++ src/utils/email.js
@@ -24,7 +24,7 @@ const sendForgetEmail = async (option) => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: true,
+    secure: port === 465,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
