66410bf70fb5cc59458551812896d377ba6bf67e
Ahmed-Abd-EL-Kareem <137029582+Ahmed-Abd-EL-Kareem@users.noreply.github.com>
2026-06-19 11:57:56 +0300

    Export sendCookie function for use in other modules

--- src/utils/sendCookie.js
+++ src/utils/sendCookie.js
@@ -9,7 +9,6 @@
 //   });
 // };
 
-// export default sendCookie;
 const sendCookie = (res, token) => {
   res.cookie("token", token, {
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
@@ -18,3 +17,4 @@ const sendCookie = (res, token) => {
     sameSite: "Lax",  // Change from "None" to "Lax" for localhost
   });
 };
+export default sendCookie;
