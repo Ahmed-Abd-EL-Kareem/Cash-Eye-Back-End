3725b461ced550088450a3073a54b46fb307167c
Ahmed-Abd-EL-Kareem <137029582+Ahmed-Abd-EL-Kareem@users.noreply.github.com>
2026-06-19 11:54:20 +0300

    Modify sendCookie function for Angular compatibility
    Updated cookie settings to allow Angular to read the token and adjusted sameSite for localhost.

--- src/utils/sendCookie.js
+++ src/utils/sendCookie.js
@@ -1,12 +1,20 @@
+// const sendCookie = (res, token) => {
+//   res.cookie("token", token, {
+//     expires: new Date(
+//       Date.now() + 7 * 24 * 60 * 60 * 1000
+//     ),
+//     httpOnly: true,
+//     secure: true,
+//     sameSite: "None",
+//   });
+// };
+
+// export default sendCookie;
 const sendCookie = (res, token) => {
   res.cookie("token", token, {
-    expires: new Date(
-      Date.now() + 7 * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
-    secure: true,
-    sameSite: "None",
+    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    httpOnly: false,  // Change to false so Angular can read it
+    secure: false,    // Must be false for localhost (HTTP)
+    sameSite: "Lax",  // Change from "None" to "Lax" for localhost
   });
 };
-
-export default sendCookie;
