bf69c88a43f09c0c41c0040be87c7590f2e04564
Ahmed-Abd-EL-Kareem <137029582+Ahmed-Abd-EL-Kareem@users.noreply.github.com>
2026-06-27 08:25:49 +0300

    Add console logs for CORS configuration
    Added console logs for CLIENT_URL and FRONTEND_PROD_URL.

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,11 +1,13 @@
 const allowedOrigins = [
+  "https://rahal-main-site.vercel.app/",
   process.env.CLIENT_URL,
   process.env.FRONTEND_URL,
   process.env.DASHBOARD_URL,
   process.env.FRONTEND_PROD_URL,
   process.env.DASHBOARD_PROD_URL,
 ].filter(Boolean);
-
+console.log("Client",  process.env.CLIENT_URL)
+console.log("Prod",  process.env.FRONTEND_PROD_URL)
 const corsOptions = {
   origin: (origin, callback) => {
     // Allow server-to-server / Postman / Stripe redirects with no Origin header
