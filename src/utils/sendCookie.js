29c86793ccb8f4594daf7bb710ddc0b30c0810b6
Ahmed-Abd-EL-Kareem <137029582+Ahmed-Abd-EL-Kareem@users.noreply.github.com>
2026-06-27 06:54:13 +0300

    Cookies Fix

--- src/utils/sendCookie.js
+++ src/utils/sendCookie.js
@@ -9,11 +9,13 @@
 //   });
 // };
 
-res.cookie("token", token, {
-  httpOnly: true,
-  secure: true,
-  sameSite: "none",
-  path: "/",
-  maxAge: 7 * 24 * 60 * 60 * 1000,
-});
+const sendCookie = (res, token) => {
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    path: "/",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+};
 export default sendCookie;
